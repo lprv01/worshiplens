953c040adc033a0cd05a3196df0fe1294cdd2448
Ludwingk Rios <ludwingk.rios@Lud-Mac.local>
2026-03-17 19:45:32 -0500

    remove duplicate dynamic export

--- app/songs/[id]/page.tsx
+++ app/songs/[id]/page.tsx
@@ -1,7 +1,5 @@
 export const dynamic = "force-dynamic"
 
-export const dynamic = "force-dynamic"
-
 import { createClient } from "@supabase/supabase-js";
 import Link from "next/link";
 import { notFound } from "next/navigation";
